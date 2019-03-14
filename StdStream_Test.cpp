b10de191092bbdc51e0e17fe64dce98482424a00
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-03-14 17:33:22 +0200

    Tests: add

---
+++ StdStream_Test.cpp
@@ -0,0 +1,123 @@
+/**
+ * \file  main.cpp
+ * \brief
+ */
+
+
+#include <iostream>
+#include "StdStream.h"
+
+//-------------------------------------------------------------------------------------------------
+int main(int, char **)
+{
+	{
+		// auto
+	}
+
+	{
+		std::pair<std::string, std::string> value;
+		std::cout << value << std::endl;
+	}
+
+#if 0
+	{
+		std::vector<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::list<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::set<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::multiset<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::deque<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::queue<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::priority_queue<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::stack<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::map<std::string, std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::multimap<std::string, std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		/// std::map<std::string, std::string, CompareT> value;
+		/// std::cout << value << std::endl;
+	}
+
+	{
+		std::array<std::string, 0> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::forward_list<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::unordered_map<std::string, std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::unordered_multimap<std::string, std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::unordered_set<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		std::unordered_multiset<std::string> value;
+		std::cout << value << std::endl;
+	}
+
+	{
+		// std::tuple<Args...> value;
+		// std::cout << value << std::endl;
+	}
+#endif
+
+    return EXIT_SUCCESS;
+}
+//-------------------------------------------------------------------------------------------------
+
+
+#if OUTPUT
+
+
+
+#endif
