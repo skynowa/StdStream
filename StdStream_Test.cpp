9b42ee46f8b9b311c6bed2b65ebca79020133c0c
skynowa <skynowa@gmail.com>
2026-06-26 00:35:50 +0300

    StdStream_Test.cpp [59+/49-] - test(s)

--- StdStream_Test.cpp
+++ StdStream_Test.cpp
@@ -4,12 +4,22 @@
  */
 
 
-#include <cassert>
 #include <cstdlib>
+#include <iostream>
 #include <sstream>
 #include <string>
 
 #include "StdStream.h"
+//-------------------------------------------------------------------------------------------------
+#define TEST(expr)                                                   \
+	do {                                                              \
+		if (!(expr)) {                                                \
+			std::cerr << "TEST failed: " #expr                        \
+					  << "\nFile: " << __FILE__                      \
+					  << "\nLine: " << __LINE__ << '\n';             \
+			std::abort();                                             \
+		}                                                             \
+	} while (false)
 //-------------------------------------------------------------------------------------------------
 template<typename T>
 class TestAllocator
@@ -72,51 +82,51 @@ std::string toString(const ValueT &value)
 	return os.str();
 }
 
-void assertContains(const std::string &value, const std::string &part)
+void testContains(const std::string &value, const std::string &part)
 {
-	assert(value.find(part) != std::string::npos);
+	TEST(value.find(part) != std::string::npos);
 }
 //-------------------------------------------------------------------------------------------------
 void testPair()
 {
 	const std::pair<std::string, std::string> emptyValue;
-	assert(toString(emptyValue) == "std::pair: {,}");
+	TEST(toString(emptyValue) == "std::pair: {,}");
 
 	const std::pair<std::string, std::string> value {"aaa", "111"};
-	assert(toString(value) == "std::pair: {aaa,111}");
+	TEST(toString(value) == "std::pair: {aaa,111}");
 }
 //-------------------------------------------------------------------------------------------------
 void testSequenceContainers()
 {
 	{
 		std::vector<std::string> emptyValue;
-		assert(toString(emptyValue) == "std::vector (size=0): {}");
+		TEST(toString(emptyValue) == "std::vector (size=0): {}");
 
 		std::vector<std::string, TestAllocator<std::string>> value {"aaa", "bbb", "ccc"};
-		assert(toString(value) == "std::vector (size=3): {aaa,bbb,ccc}");
+		TEST(toString(value) == "std::vector (size=3): {aaa,bbb,ccc}");
 	}
 
 	{
 		std::list<std::string, TestAllocator<std::string>> value {"aaa1", "bbb2", "ccc3"};
-		assert(toString(value) == "std::list (size=3): {aaa1,bbb2,ccc3}");
+		TEST(toString(value) == "std::list (size=3): {aaa1,bbb2,ccc3}");
 	}
 
 	{
 		std::deque<std::string, TestAllocator<std::string>> value {"aaa", "bbb", "ccc"};
-		assert(toString(value) == "std::deque (size=3): {aaa,bbb,ccc}");
+		TEST(toString(value) == "std::deque (size=3): {aaa,bbb,ccc}");
 	}
 
 	{
 		std::forward_list<std::string, TestAllocator<std::string>> value {"aaa", "bbb", "ccc"};
-		assert(toString(value) == "std::forward_list (size=3): {aaa,bbb,ccc}");
+		TEST(toString(value) == "std::forward_list (size=3): {aaa,bbb,ccc}");
 	}
 
 	{
 		std::array<std::string, 0> emptyValue;
-		assert(toString(emptyValue) == "std::array (size=0): {}");
+		TEST(toString(emptyValue) == "std::array (size=0): {}");
 
 		std::array<std::string, 3> value {"a", "b", "c"};
-		assert(toString(value) == "std::array (size=3): {a,b,c}");
+		TEST(toString(value) == "std::array (size=3): {a,b,c}");
 	}
 }
 //-------------------------------------------------------------------------------------------------
@@ -124,24 +134,24 @@ void testOrderedAssociativeContainers()
 {
 	{
 		std::set<int, std::greater<int>, TestAllocator<int>> value {1, 2, 2, 3};
-		assert(toString(value) == "std::set (size=3): {3,2,1}");
+		TEST(toString(value) == "std::set (size=3): {3,2,1}");
 	}
 
 	{
 		std::multiset<int, std::greater<int>, TestAllocator<int>> value {1, 2, 2, 3};
-		assert(toString(value) == "std::multiset (size=4): {3,2,2,1}");
+		TEST(toString(value) == "std::multiset (size=4): {3,2,2,1}");
 	}
 
 	{
 		using ValueT = std::pair<const int, int>;
 		std::map<int, int, std::greater<int>, TestAllocator<ValueT>> value {{1, 10}, {2, 20}};
-		assert(toString(value) == "std::map (size=2): {std::pair: {2,20}\nstd::pair: {1,10}}");
+		TEST(toString(value) == "std::map (size=2): {std::pair: {2,20}\nstd::pair: {1,10}}");
 	}
 
 	{
 		using ValueT = std::pair<const int, int>;
 		std::multimap<int, int, std::greater<int>, TestAllocator<ValueT>> value {{1, 10}, {2, 20}, {2, 22}};
-		assert(toString(value) == "std::multimap (size=3): {std::pair: {2,20}\nstd::pair: {2,22}\nstd::pair: {1,10}}");
+		TEST(toString(value) == "std::multimap (size=3): {std::pair: {2,20}\nstd::pair: {2,22}\nstd::pair: {1,10}}");
 	}
 }
 //-------------------------------------------------------------------------------------------------
@@ -152,9 +162,9 @@ void testUnorderedAssociativeContainers()
 		std::unordered_map<int, int, IntHash, IntEqual, TestAllocator<ValueT>> value {{1, 10}, {2, 20}};
 		const std::string out = toString(value);
 
-		assertContains(out, "std::unordered_map (size=2): {");
-		assertContains(out, "std::pair: {1,10}");
-		assertContains(out, "std::pair: {2,20}");
+		testContains(out, "std::unordered_map (size=2): {");
+		testContains(out, "std::pair: {1,10}");
+		testContains(out, "std::pair: {2,20}");
 	}
 
 	{
@@ -162,30 +172,30 @@ void testUnorderedAssociativeContainers()
 		std::unordered_multimap<int, int, IntHash, IntEqual, TestAllocator<ValueT>> value {{1, 10}, {2, 20}, {2, 22}};
 		const std::string out = toString(value);
 
-		assertContains(out, "std::unordered_multimap (size=3): {");
-		assertContains(out, "std::pair: {1,10}");
-		assertContains(out, "std::pair: {2,20}");
-		assertContains(out, "std::pair: {2,22}");
+		testContains(out, "std::unordered_multimap (size=3): {");
+		testContains(out, "std::pair: {1,10}");
+		testContains(out, "std::pair: {2,20}");
+		testContains(out, "std::pair: {2,22}");
 	}
 
 	{
 		std::unordered_set<int, IntHash, IntEqual, TestAllocator<int>> value {1, 2, 2, 3};
 		const std::string out = toString(value);
 
-		assertContains(out, "std::unordered_set (size=3): {");
-		assertContains(out, "1");
-		assertContains(out, "2");
-		assertContains(out, "3");
+		testContains(out, "std::unordered_set (size=3): {");
+		testContains(out, "1");
+		testContains(out, "2");
+		testContains(out, "3");
 	}
 
 	{
 		std::unordered_multiset<int, IntHash, IntEqual, TestAllocator<int>> value {1, 2, 2, 3};
 		const std::string out = toString(value);
 
-		assertContains(out, "std::unordered_multiset (size=4): {");
-		assertContains(out, "1");
-		assertContains(out, "2");
-		assertContains(out, "3");
+		testContains(out, "std::unordered_multiset (size=4): {");
+		testContains(out, "1");
+		testContains(out, "2");
+		testContains(out, "3");
 	}
 }
 //-------------------------------------------------------------------------------------------------
@@ -197,10 +207,10 @@ void testContainerAdaptors()
 		value.push("bbb");
 		value.push("ccc");
 
-		assert(toString(value) == "std::queue (size=3): {aaa,bbb,ccc}");
-		assert(value.size() == 3);
-		assert(value.front() == "aaa");
-		assert(value.back() == "ccc");
+		TEST(toString(value) == "std::queue (size=3): {aaa,bbb,ccc}");
+		TEST(value.size() == 3);
+		TEST(value.front() == "aaa");
+		TEST(value.back() == "ccc");
 	}
 
 	{
@@ -209,9 +219,9 @@ void testContainerAdaptors()
 		value.push(1);
 		value.push(2);
 
-		assert(toString(value) == "std::priority_queue (size=3): {1,2,3}");
-		assert(value.size() == 3);
-		assert(value.top() == 1);
+		TEST(toString(value) == "std::priority_queue (size=3): {1,2,3}");
+		TEST(value.size() == 3);
+		TEST(value.top() == 1);
 	}
 
 	{
@@ -220,19 +230,19 @@ void testContainerAdaptors()
 		value.push("bbb");
 		value.push("ccc");
 
-		assert(toString(value) == "std::stack (size=3): {aaa,bbb,ccc}");
-		assert(value.size() == 3);
-		assert(value.top() == "ccc");
+		TEST(toString(value) == "std::stack (size=3): {aaa,bbb,ccc}");
+		TEST(value.size() == 3);
+		TEST(value.top() == "ccc");
 	}
 }
 //-------------------------------------------------------------------------------------------------
 void testTuple()
 {
 	const std::tuple<std::string, double, int, char> value = std::make_tuple("test", 3.1, 14, 'y');
-	assert(toString(value) == "std::tuple (size=4): {test,3.1,14,y}");
+	TEST(toString(value) == "std::tuple (size=4): {test,3.1,14,y}");
 
 	const std::tuple<> emptyValue;
-	assert(toString(emptyValue) == "std::tuple (size=0): {}");
+	TEST(toString(emptyValue) == "std::tuple (size=0): {}");
 }
 //-------------------------------------------------------------------------------------------------
 void testTracePtr()
@@ -243,8 +253,8 @@ void testTracePtr()
 		os << STD_TRACE_PTR(value);
 		const std::string out = os.str();
 
-		assertContains(out, "value: {");
-		assertContains(out, "nullptr");
+		testContains(out, "value: {");
+		testContains(out, "nullptr");
 	}
 
 	{
@@ -254,8 +264,8 @@ void testTracePtr()
 		os << STD_TRACE_PTR(value);
 		const std::string out = os.str();
 
-		assertContains(out, "value: {");
-		assertContains(out, ", abc}");
+		testContains(out, "value: {");
+		testContains(out, ", abc}");
 	}
 
 	{
@@ -265,8 +275,8 @@ void testTracePtr()
 		os << STD_TRACE_PTR(value);
 		const std::string out = os.str();
 
-		assertContains(out, "value: {");
-		assertContains(out, ", 7}");
+		testContains(out, "value: {");
+		testContains(out, ", 7}");
 	}
 }
 //-------------------------------------------------------------------------------------------------
